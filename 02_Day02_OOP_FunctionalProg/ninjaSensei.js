9669e668ac7a566737f72f3112269dfc8763d405
Amine <mohamedamin.benmakhlouf@gmail.com>
2023-08-29 16:21:51 +0100

    ninjaSensei

---
+++ 02_Day02_OOP_FunctionalProg/ninjaSensei.js
@@ -0,0 +1,56 @@
+class Ninja
+{
+    constructor(name,health,speed=3,strength=3)
+    {
+        this.name =name
+        this.health = health
+        this.speed = speed
+        this.strength = strength
+    }
+    
+    sayName()
+    {
+        console.log(`Ninja name is ${this.name}`)
+        return this
+    }
+    showStats()
+    {
+        console.log(`Ninja name is ${this.name}\nhis strength equal to ${this.strength}\nhis speed is ${this.speed}\nand his health equal to ${this.health}`)
+        return this
+    }
+
+    drinkSake()
+    {
+        this.health+=10
+        return this
+    }
+}
+const AlanNinja = new Ninja("Alan",50,30)
+// AlanNinja.sayName()
+// AlanNinja.showStats()
+// AlanNinja.drinkSake().showStats()
+
+class Sensei extends Ninja
+{
+    constructor(name,health=200,speed=10,strength=10,wisdom=10)
+    {
+        super(name,health,speed,strength)
+        this.wisdom = wisdom
+    }
+    speakWisdom()
+    {
+        this.drinkSake()
+        return this
+    }
+}
+
+
+
+// example output
+const superSensei = new Sensei("Master Splinter");
+// superSensei.speakWisdom();
+const health = superSensei.speakWisdom();
+console.log(health);
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
