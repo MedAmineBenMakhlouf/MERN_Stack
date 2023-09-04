78c2a93a4d2e8ddb744c5eeae5741588765b187e
Amine <mohamedamin.benmakhlouf@gmail.com>
2023-09-04 18:20:20 +0100

    update width height

--- 05_Day05_FunctionalComponent/Practice/box_generator/src/components/Add_Color.jsx
+++ 05_Day05_FunctionalComponent/Practice/box_generator/src/components/Add_Color.jsx
@@ -1,23 +1,40 @@
 import React, { useState } from 'react'
 
-const colorName =""
 const Add_Color = (props) => {
     const [inputColor, setColor] = useState("")
-
-    
+    const [width, setWidth] = useState("")
+    const [height, setHeight] = useState("")
     const handleColor = (e) => {
         e.preventDefault()
-        props.Add(inputColor)
+        props.Add({inputColor:inputColor,width:width,height:height})
         setColor("")
+        setWidth("")
+        setHeight("")
+    }
+    const handleWidth = (e)=>{
+        setWidth(parseInt(e.target.value))
+    }
+    const handleHeight = (e)=>{
+        setHeight(parseInt(e.target.value))
     }
     return (
         <>
             <form onSubmit={handleColor}>
-                <p>Add color name: <input type="text"
-
-                onChange={(e)=>setColor(e.target.value)} 
-                value={inputColor}/></p>
-
+                <p>
+                    Add color name: <input type="text"
+                        onChange={(e) => setColor(e.target.value)}
+                        value={inputColor} />
+                </p>
+                <p>
+                    Add Width: <input type="number" onChange={(e)=>handleWidth(e)}
+                    value={width}
+                    />
+                </p>
+                <p>
+                    Add Height: <input type="number" onChange={(e)=>handleHeight(e)}
+                    value={height}
+                    />
+                </p>
                 <button>Add </button>
             </form>
         </>
--- 05_Day05_FunctionalComponent/Practice/box_generator/src/components/Show_Color.jsx
+++ 05_Day05_FunctionalComponent/Practice/box_generator/src/components/Show_Color.jsx
@@ -5,7 +5,8 @@ const Show_Color = ({ colors }) => {
         <div className='container_color' >
             {colors.map((c, i) => {
                 return (
-                    <div className='color' key={i} style={{ backgroundColor: c }}></div>
+                    <div className='color' key={i} 
+                    style={{ backgroundColor: c.inputColor, width: c.width, height: c.height }}></div>
                 )
             }
             )}
